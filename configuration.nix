b16084f099fc64efd594822dc371454e763b220b
Stella Wuppermann <stella.wuppermann99@googlemail.com>
2023-12-17 17:54:39 +0100

    set hostname

--- configuration.nix
+++ configuration.nix
@@ -19,7 +19,7 @@
     "/crypto_keyfile.bin" = null;
   };
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "coulon"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
