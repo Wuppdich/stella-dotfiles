767937c00e4fce1bc142c55b2d7cc075636a1995
Stella Wuppermann <stella.wuppermann99@googlemail.com>
2023-12-20 12:43:27 +0100

    changed default editor to nvim

--- configuration.nix
+++ configuration.nix
@@ -152,6 +152,10 @@
   #  wget
   ];
 
+  environment.variables = {
+    EDITOR = "nvim";
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
