0f716e1a5ebbe2e593bb826016a80d3a2befe73f
Stella Wuppermann <stella.wuppermann99@googlemail.com>
2023-12-30 12:24:43 +0100

    added font via google-fonts

--- configuration.nix
+++ configuration.nix
@@ -123,6 +123,10 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  fonts.packages = with pkgs; [
+    (google-fonts.override { fonts = [ "Pathway Gothic One" ]; })
+  ];
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.alice = {
     isNormalUser = true;
