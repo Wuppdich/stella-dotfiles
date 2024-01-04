11602631a1a853e08cd67d862b75b09836c8a5fe
Stella Wuppermann <stella.wuppermann99@googlemail.com>
2024-01-04 12:52:47 +0100

    changed to the open source nvidia driver -> fixes flickering

--- configuration.nix
+++ configuration.nix
@@ -59,13 +59,16 @@
   hardware.nvidia = {
 
     # Modesetting is required.
-    modesetting.enable = true;
+    modesetting.enable = false;
 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
     powerManagement.enable = false;
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
     powerManagement.finegrained = false;
+
+    # ensures all GPUs stay away during headless mode
+    nvidiaPersistenced = true;
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
@@ -74,7 +77,7 @@
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
+    open = true;
 
     # Enable the Nvidia settings menu,
 	  # accessible via `nvidia-settings`.
