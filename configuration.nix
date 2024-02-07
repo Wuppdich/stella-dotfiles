7b5fc61f2ef3e8383f7cd1c6da6a9b3414aab807
Stella Wuppermann <stella.wuppermann99@googlemail.com>
2024-02-07 09:11:07 +0100

    fix audio crackling

--- configuration.nix
+++ configuration.nix
@@ -187,6 +187,29 @@
   #  wget
   ];
 
+  environment.etc = {
+    "wireplumber/main.lua.d/51-disable-suspension.lua" = {
+      text = ''
+        table.insert (alsa_monitor.rules, {
+          matches = {
+            {
+              -- Matches all sources.
+              { "node.name", "matches", "alsa_input.*" },
+            },
+            {
+              -- Matches all sinks.
+              { "node.name", "matches", "alsa_output.*" },
+            },
+          },
+          apply_properties = {
+            ["session.suspend-timeout-seconds"] = 300,  -- 0 disables suspend
+          },
+        })
+      '';
+      mode = "0554";
+    };
+  };
+
   environment.variables = {
     EDITOR = "nvim";
   };
