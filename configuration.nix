fb25de27f097618ab27fc8701ef3ce35f17a35d3
Stella Wuppermann <stella.wuppermann99@googlemail.com>
2024-05-17 04:06:37 +0200

    compacten nvidia config section

--- configuration.nix
+++ configuration.nix
@@ -94,19 +94,12 @@ in
   };
 
   hardware.nvidia = {
-
     # Modesetting is required. TODO: But switching this to "true" makes the login screen fail.
     modesetting.enable = false;
-
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
     powerManagement.enable = true;
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
     # ensures all GPUs stay awake during headless mode
     nvidiaPersistenced = true;
-
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
     # Support is limited to the Turing and later architectures. Full list of 
@@ -115,11 +108,9 @@ in
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
     open = true;
-
     # Enable the Nvidia settings menu,
 	  # accessible via `nvidia-settings`.
     nvidiaSettings = true;
-
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
