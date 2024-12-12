cb5f72bbbc8aa26aa33569660d59bff2d98e889f
Stella Wuppermann <stella.wuppermann99@googlemail.com>
2024-12-12 23:03:29 +0100

    set fish greeting to empty and enable transient prompt

--- home-manager.nix
+++ home-manager.nix
@@ -146,6 +146,15 @@
           };
         };
       };
+      programs.fish = {
+        enable = true;
+        shellInit = ''
+          set fish_greeting
+        '';
+        shellInitLast = ''
+          enable_transience
+        '';
+      };
 
       # do not change this without good reason! 
       home.stateVersion = "24.05";
