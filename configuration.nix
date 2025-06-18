45319f15351726140389d16b250a501eb3aee3af
Stella Wuppermann <stella.wuppermann99@googlemail.com>
2025-06-18 18:34:40 +0200

    The option `hardware.pulseaudio' has been renamed to `services.pulseaudio'.
    (cherry picked from commit ed51de4af42aff46627005eb9e73a9b5e8ae5fa5)

--- configuration.nix
+++ configuration.nix
@@ -118,7 +118,6 @@
   console.keyMap = "de";
 
   hardware = {
-    pulseaudio.enable = false;
     graphics = {
       enable32Bit = true;
       extraPackages = with pkgs; [
@@ -128,17 +127,20 @@
   };
   security.rtkit.enable = true;
   # Enable sound with pipewire.
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+  services = {
+      pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
+    pulseaudio.enable = false;
   };
 
   musnix.enable = true;
