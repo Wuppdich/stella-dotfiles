2435181d88523ec7e66d6a22e23ce369863e1ad9
Stella Wuppermann <stella.wuppermann99@googlemail.com>
2025-12-11 17:42:00 +0100

    cuda cache moved to nixos-cuda.org

--- nix.nix
+++ nix.nix
@@ -14,11 +14,11 @@
     settings = {
       # additional binary caches to use
       substituters = [
-        "https://cuda-maintainers.cachix.org"
+        "https://cache.nixos-cuda.org"
         "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
-        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+        "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
       experimental-features = "nix-command flakes";
