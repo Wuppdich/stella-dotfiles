721b59aa250fa2f4025aa255c3c6a451bcc51c1b
Stella Wuppermann <stella.wuppermann99@googlemail.com>
2026-04-18 22:09:16 +0200

    move blender thumbnailer to user config

--- alice.nix
+++ alice.nix
@@ -58,6 +58,17 @@
       (blender.override {
         cudaSupport = true;
       })
+      # blend file thumbnailer
+      (pkgs.writeTextFile {
+        name = "blender thumbnails";
+        text = ''
+          [Thumbnailer Entry]
+          TryExec=blender-thumbnailer
+          Exec=blender-thumbnailer %i %o
+          MimeType=application/x-blender;
+        '';
+        destination = "/share/thumbnailers/blender.thumbnailer";
+      })
       freecad
       (gimp-with-plugins.override {
         plugins = with gimpPlugins; [
--- configuration.nix
+++ configuration.nix
@@ -118,18 +118,6 @@
     # tools
     git
     btop
-    # settings
-    # blend file thumbnailer
-    (pkgs.writeTextFile {
-      name = "blender thumbnails";
-      text = ''
-        [Thumbnailer Entry]
-        TryExec=blender-thumbnailer
-        Exec=blender-thumbnailer %i %o
-        MimeType=application/x-blender;
-      '';
-      destination = "/share/thumbnailers/blender.thumbnailer";
-    })
   ];
 
   programs = {
