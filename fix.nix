971f598f441ea4312a891993d96fdd32d2112645
Stella Wuppermann <stella.wuppermann99@googlemail.com>
2026-04-21 10:22:35 +0200

    format fix.nix

--- fix.nix
+++ fix.nix
@@ -1,33 +1,26 @@
 {
-  config,
-  pkgs,
-  lib,
   ...
 }:
 {
-  environment = {
-    # fix crackling noises
-    etc = {
-      "wireplumber/main.lua.d/51-disable-suspension.lua" = {
-        text = ''
-          table.insert (alsa_monitor.rules, {
-            matches = {
-              {
-                -- Matches all sources.
-                { "node.name", "matches", "alsa_input.*" },
-              },
-              {
-                -- Matches all sinks.
-                { "node.name", "matches", "alsa_output.*" },
-              },
-            },
-            apply_properties = {
-              ["session.suspend-timeout-seconds"] = 300,  -- 0 disables suspend
-            },
-          })
-        '';
-        mode = "0554";
-      };
-    };
+  # fix crackling noises
+  environment.etc."wireplumber/main.lua.d/51-disable-suspension.lua" = {
+    text = ''
+      table.insert (alsa_monitor.rules, {
+        matches = {
+          {
+            -- Matches all sources.
+            { "node.name", "matches", "alsa_input.*" },
+          },
+          {
+            -- Matches all sinks.
+            { "node.name", "matches", "alsa_output.*" },
+          },
+        },
+        apply_properties = {
+          ["session.suspend-timeout-seconds"] = 300,  -- 0 disables suspend
+        },
+      })
+    '';
+    mode = "0554";
   };
 }
