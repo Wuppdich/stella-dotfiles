245e2b4b5e0c5589ebb15d0c1541b34c01d2e784
Stella Wuppermann <stella.wuppermann99@googlemail.com>
2026-06-08 00:50:40 +0200

    reorder shell packages, added comments

--- shell.nix
+++ shell.nix
@@ -2,15 +2,18 @@
 
 pkgs.mkShell {
   packages = with pkgs; [
-    lix-diff
-    just
-    just-lsp
-    scc
-    sops
-    age
-    ssh-to-age
-    nix-output-monitor
-    rich-cli
+    sops # secrets management
+    age # secrets encryption
+    ssh-to-age # age key generation. not used
+
+    lix-diff # diffs derivations
+    just # command runner
+    scc # code stats
+    nix-output-monitor # pretty build output
+    rich-cli # pretty json ouput
+
+    # (this should be moved into the editor environment)
+    just-lsp # just language server
   ];
 }
 
