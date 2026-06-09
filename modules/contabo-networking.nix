e83c2f52ea93ca7936f639d747f2cb683469ee88
Stella Wuppermann <stella.wuppermann99@googlemail.com>
2026-06-09 22:34:09 +0200

    fully configurable contabo-networking implementation with (rough) regex checking

--- modules/contabo-networking.nix
+++ modules/contabo-networking.nix
@@ -1,63 +1,96 @@
 { config, lib, ... }:
 let
+  inherit (builtins) elemAt split;
   inherit (lib) mkEnableOption mkOption mkIf;
-  inherit (lib.types) str listOf;
+  inherit (lib.types) str listOf strMatching;
+
   cfg = config.networking.contabo;
+
+  # Gateway assumed by setting the last byte of the ip to 1
+  mkGateway = address: (elemAt (split "[[:digit:]]{1,3}\/[[:digit:]]{1,2}$" address) 0) + "1";
 in
 {
   options.networking.contabo = {
     enable = mkEnableOption "contabo networking";
+
     mac = mkOption {
-      # TODO: match regex: https://wiki.nixos.org/wiki/NixOS_modules#Examples
-      type = str;
+      type = strMatching "^([[:xdigit:]]{2}:){5}[[:xdigit:]]{2}$";
+      example = "12:23:45:67:89:AB";
     };
-    addresses = mkOption {
-      # TODO: match regex: https://wiki.nixos.org/wiki/NixOS_modules#Examples
+
+    addrIPv4 = mkOption {
+      # half-arsed implementation. posix regex is really annoying >.<
+      type = strMatching "^([[:digit:]]{1,3}.?){4}\/[[:digit:]]{1,2}$";
+      example = "192.178.1.64/24";
+    };
+
+    addrIPv6 = mkOption {
+      # half-arsed implementation. posix regex is really annoying >.<
+      type = strMatching "^([[:xdigit:]]{0,4}:?|::){2,8}\/[[:digit:]]{1,2}$";
+      example = "12ab:34cd:56ef:78ff::1/64";
+    };
+
+    address = mkOption {
       type = listOf str;
+      default = [
+        cfg.addrIPv4
+        cfg.addrIPv6
+      ];
     };
-    # TODO: define IPv4 gateway option, with generated as default
-    # TODO: define routes option, with generated routes as default
-    # TODO: define dns with contabo DNS as default.
+
+    gatewayIPv4 = mkOption {
+      # half-arsed implementation. posix regex is really annoying >.<
+      type = strMatching "^([[:digit:]]{1,3}.?){4}$";
+      default = mkGateway cfg.addrIPv4;
+      example = "192.178.1.1";
+    };
+
+    gatewayIPv6 = mkOption {
+      # half-arsed implementation. posix regex is really annoying >.<
+      type = strMatching "^([[:xdigit:]]{0,4}:?|::){2,8}$";
+      default = "fe80::1";
+      example = "12ab:34cd:56ef:78ff::1";
+    };
+
+    routes = mkOption {
+      default = [
+        # the "Destinatin=default" part can be omitted
+        {
+          Gateway = cfg.gatewayIPv4;
+          GatewayOnLink = true;
+        }
+        {
+          Gateway = cfg.gatewayIPv6;
+          GatewayOnLink = true;
+        }
+      ];
+    };
+
+    dns = mkOption {
+      type = listOf str;
+      default = [
+        "213.136.95.10"
+        "213.136.95.11"
+        "2a02:c207::1:53"
+      ];
+      example = [
+        "8.8.8.8"
+        "8.8.4.4"
+        "2001:4860:4860::8888"
+      ];
+    };
   };
 
   config = mkIf cfg.enable {
     networking.useDHCP = false;
     systemd.network = {
       enable = true;
-      networks."10-contabo-network" =
-        let
-          # Gateway assumed by setting the last byte of the ip to 1
-          mkGateway =
-            address: (builtins.elemAt (builtins.split "[[:digit:]]{1,3}\/[[:digit:]]{1,2}$" address) 0) + "1";
-          # values from `netplan get` and /run/systemd/network/*.{network|link} on the original server
-          netplan = {
-            dns = [
-              "213.136.95.10"
-              "213.136.95.11"
-              "2a02:c207::1:53"
-            ];
-            routes = [
-              # the "Destinatin=default" part can be omitted
-              {
-                # FIXME: IPv4 address assumed to be in first position
-                Gateway = mkGateway (builtins.elemAt cfg.addresses 0);
-                GatewayOnLink = true;
-              }
-              {
-                Gateway = "fe80::1";
-                GatewayOnLink = true;
-              }
-            ];
-          };
-        in
-        {
-          enable = true;
-          matchConfig.PermanentMACAddress = cfg.mac;
-          address = cfg.addresses;
-          dns = netplan.dns;
-          routes = netplan.routes;
-          linkConfig.RequiredForOnline = "routable";
-        };
+      networks."10-contabo-network" = {
+        enable = true;
+        matchConfig.PermanentMACAddress = cfg.mac;
+        inherit (cfg) address routes dns;
+        linkConfig.RequiredForOnline = "routable";
+      };
     };
   };
 }
